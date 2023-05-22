7891d7e2b232c5f95c7feaba333bcf5850aa20e8
abarbaro <abarbaro@redhat.com>
2023-05-22 16:03:52 +0200

    add cypress resources

--- cypress/cypress/e2e/test.cy.js
+++ cypress/cypress/e2e/test.cy.js
@@ -1,14 +1,13 @@
 describe('template spec', () => {
   it('passes', () => {
     cy.visit(Cypress.env('SPI_OAUTH_URL'))
+    cy.window().then((win) => { 
+      win.stop();
+    });
     cy.origin('https://github.com/login', () => {
       cy.get('#login_field').type(Cypress.env('GH_USER'));
       cy.get('#password').type(Cypress.env('GH_PASSWORD'));
       cy.get('.btn').click();
     })
-    
-    /*cy.task("generateToken", "some").then(token => {
-      cy.get("#app_totp").type(token);
-    });*/
   })
 })

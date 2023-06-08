9deb5e589acd44ef2a3a8e068277a01657301596
abarbaro <abarbaro@redhat.com>
2023-06-08 15:23:42 +0200

    fix tests

--- cypress/cypress/e2e/spec.cy.js
+++ cypress/cypress/e2e/spec.cy.js
@@ -1,37 +1,30 @@
 describe('template spec', () => {
+  it('check variables', () => {   
+    console.log(Cypress.env('SPI_OAUTH_URL'))
+    expect(Cypress.env('GH_USER')).to.not.be.empty
+    expect(Cypress.env('GH_PASSWORD')).to.not.be.empty
+    expect(Cypress.env('GH_2FA_CODE')).to.not.be.empty
+    expect(Cypress.env('SPI_OAUTH_URL')).to.not.be.empty
+  })
+
   it('passes', () => {   
-    if(Cypress.env('SPI_OAUTH_URL')==""){
-      cy.task('log', 'SPI_OAUTH_URL is empty')
-      throw new Error("SPI_OAUTH_URL is empty")
-    }
-    if(Cypress.env('GH_USER')==""){
-       cy.task('log', 'GH_USER is empty')
-      throw new Error("GH_USER is empty")
-    }
-    if(Cypress.env('GH_PASSWORD')==""){
-       cy.task('log', 'GH_PASSWORD is empty')
-      throw new Error("GH_PASSWORD is empty")
-    }
-    if(Cypress.env('GH_2FA_CODE')==""){
-       cy.task('log', 'GH_2FA_CODE is empty')
-      throw new Error("GH_2FA_CODE is empty")
-    }
-    
+
     cy.task('log', 'Visiting '+Cypress.env('SPI_OAUTH_URL'))
     cy.visit(Cypress.env('SPI_OAUTH_URL'))
     
     cy.url().then((url) => {
       cy.task('log', 'Current URL is: ' + url)
     })
-
+    
     cy.origin('https://github.com/login', () => {
-      cy.url().then((url) => {
-        cy.task('log', 'Current Github URL is: ' + url)
-      })
       
       cy.get('#login_field').should('exist')
       cy.get('#password').should('exist')
       cy.get('input[type="submit"][name="commit"]').should('exist')
+
+      cy.url().then((url) => {
+        cy.task('log', 'Current Github URL is: ' + url)
+      })
 
       cy.get('#login_field').type(Cypress.env('GH_USER'));
       cy.get('#password').type(Cypress.env('GH_PASSWORD'));
@@ -40,10 +33,7 @@ describe('template spec', () => {
       cy.task("generateToken", Cypress.env('GH_2FA_CODE')).then(token => {
         cy.get("#app_totp").type(token);
         cy.task('log', 'Generated token')
-        if(token==""){
-          cy.task('log', 'token is empty')
-          throw new Error("token is empty")
-        }
+        expect(token).to.not.be.empty
       });
       
       cy.get('body').then(($el) => {
@@ -54,17 +44,16 @@ describe('template spec', () => {
           cy.task('log', 'No need to authorize app')
         }
       });
+
     })
-    
-    
-    
-  })
-  it('passes', () => {   
-    cy.url().then((url) => {
-        cy.task('log', 'Current Github URL is: ' + url)  
-    })
+
     cy.location('pathname')
       .should('include', '/callback_success')
+    cy.url().then((url) => {
+      cy.task('log', 'Current Github URL is: ' + url)  
+    })
+
   })
+
 })
 
